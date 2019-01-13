d66fd47e8b1f55eb52c9603f17c6d292c11285f0
Edgar Martinez <leetgardo@gmail.com>
2019-01-12 19:22:30 -0800

    Added some javascript files used for automation via node.

---
+++ automation/update-ts-refs.js
@@ -0,0 +1,27 @@
+const getDeps = require(__dirname + '/get-ts-deps.js');
+const path = require('path');
+
+async function main() {
+    const originFolder = path.resolve(process.cwd() + '/' + process.argv[2]);
+    const { npmDependencies, tsReferences } = await getDeps.getDependencies(originFolder, path.resolve(originFolder, 'package.json'));
+    
+    const dedupedNpmDependencies = getDeps.dedupeDependencies(npmDependencies);
+
+    const toInstall = [];
+
+    for(const packageName in dedupedNpmDependencies)
+        for(const version of dedupedNpmDependencies[packageName])
+            toInstall.push(packageName + '@' + version);
+
+    const npmInstallationCommands = [
+        `npm i ${originFolder} -g ${toInstall.join(' ')}`
+    ]
+
+    console.log(JSON.stringify({
+        npmInstallationCommands,
+        npmDependencies,
+        tsReferences
+    }, null, '   '));
+}
+
+main();
