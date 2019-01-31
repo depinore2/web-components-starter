1048beff41a4c14444c34cb58fb5d1e66c111e8c
Edgar Martinez <edgmartinez@altamed.org>
2019-01-31 15:49:14 -0800

    Fixed an issue where shims were not loading sequentially.

--- automation/run.ps1
+++ automation/run.ps1
@@ -3,11 +3,11 @@ set-location "$psscriptroot/../www"
 & "./automation/build.ps1"
 
 #do it again, but in watch mode now.
-$tscCmd = "`'node $(resolve-path ./node_modules/typescript/lib/tsc.js) -p $(resolve-path tsconfig.json) --watch`'"
+$tscCmd = "`'$(resolve-path ./node_modules/.bin/tsc) -p $(resolve-path tsconfig.json) --watch`'"
 $browserifyCmd = "`'pwsh $(resolve-path "./automation/browserify.ps1") -watchMode`'"
-$httpCmd = "`'http-server -s -o -c-1`'"
+$httpCmd = "`'$(resolve-path ./node_modules/.bin/http-server) -s -o -c-1`'"
 
-$wholeCommand = "node $(resolve-path ./node_modules/concurrently/bin/concurrently.js) $tscCmd $browserifyCmd $httpCmd"
+$wholeCommand = "$(resolve-path ./node_modules/.bin/concurrently) $tscCmd $browserifyCmd $httpCmd"
 
 write-host $wholeCommand
 
